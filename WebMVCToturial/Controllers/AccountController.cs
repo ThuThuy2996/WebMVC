d485c3811660b831f833a5e10d8d4d5e646cd18e
Nguyễn Thị Thu Thuỷ <nguyenthithuthuy0996@gmail.com>
2023-07-21 16:39:18 +0700

    Register

--- WebMVCToturial/Controllers/AccountController.cs
+++ WebMVCToturial/Controllers/AccountController.cs
@@ -32,17 +32,48 @@ namespace WebMVCToturial.Controllers
             if (user != null)
             {
                 var checkPass = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
-                if(checkPass)
+                if (checkPass)
                 {
                     var resultLogin = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
                     if (resultLogin.Succeeded)
                     {
                         return RedirectToAction("Index", "Race");
                     }
-                }              
+                }
             }
             TempData["Error"] = "Wrong credentials! Please try again.";
             return View(loginViewModel);
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+            var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerViewModel);
+            }
+            var newUser = new AppUser()
+            {
+                Email = registerViewModel.EmailAddress,
+                UserName = registerViewModel.EmailAddress
+            };
+            var result = await _userManager.CreateAsync(newUser,registerViewModel.Password);
+            if (result.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return RedirectToAction("Home");
+        }
+        public async Task<IActionResult> LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Race");
         }
     }
 }
